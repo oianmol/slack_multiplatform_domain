1ffa860eb83e7e9040db6211adceca26e400300c
anmol verma <anmol.verma4@gmail.com>
2022-10-19 15:33:40 +0530

    move to domain!

--- src/commonMain/kotlin/dev/baseio/slackdomain/datasources/remote/workspaces/SKNetworkSourceWorkspaces.kt
+++ src/commonMain/kotlin/dev/baseio/slackdomain/datasources/remote/workspaces/SKNetworkSourceWorkspaces.kt
@@ -1,6 +1,7 @@
 package dev.baseio.slackdomain.datasources.remote.workspaces
 
 import dev.baseio.slackdomain.model.users.DomainLayerUsers
+
 interface SKNetworkSourceWorkspaces {
   suspend fun saveWorkspace(email: String, password: String, domain: String): DomainLayerUsers.SKAuthResult
 }
