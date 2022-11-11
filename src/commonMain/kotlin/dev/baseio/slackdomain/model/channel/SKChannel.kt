5cd7981339682d69f73e6953a99dabee8e765796
anmol verma <anmol.verma4@gmail.com>
2022-11-11 16:07:42 +0530

    wip support: keys for individual channels

--- src/commonMain/kotlin/dev/baseio/slackdomain/model/channel/SKChannel.kt
+++ src/commonMain/kotlin/dev/baseio/slackdomain/model/channel/SKChannel.kt
@@ -21,7 +21,7 @@ interface DomainLayerChannels {
       val createdDate: Long = Clock.System.now().toEpochMilliseconds(),
       val modifiedDate: Long = Clock.System.now().toEpochMilliseconds(),
       val deleted: Boolean,
-      val userPublicKey: DomainLayerUsers.SKUserPublicKey
+      val userPublicKey: DomainLayerUsers.SKUserPublicKey = DomainLayerUsers.SKUserPublicKey(arrayOf<Byte>().toByteArray())
     ) : SKChannel(workId, uuid, publicKey = userPublicKey)
 
     data class SkGroupChannel(
@@ -32,7 +32,7 @@ interface DomainLayerChannels {
       val modifiedDate: Long = Clock.System.now().toEpochMilliseconds(),
       var avatarUrl: String?,
       val deleted: Boolean,
-      val userPublicKey: DomainLayerUsers.SKUserPublicKey
+      val userPublicKey: DomainLayerUsers.SKUserPublicKey = DomainLayerUsers.SKUserPublicKey(arrayOf<Byte>().toByteArray())
     ) : SKChannel(workId, uuid, channelName = name, pictureUrl = avatarUrl, publicKey = userPublicKey)
   }
 
