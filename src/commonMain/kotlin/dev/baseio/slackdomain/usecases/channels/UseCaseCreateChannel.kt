7101b019026dfc1239a6344b7c3a022872639d2a
anmol verma <anmol.verma4@gmail.com>
2022-11-11 20:16:09 +0530

    wip support: keys for individual channels

--- src/commonMain/kotlin/dev/baseio/slackdomain/usecases/channels/UseCaseCreateChannel.kt
+++ src/commonMain/kotlin/dev/baseio/slackdomain/usecases/channels/UseCaseCreateChannel.kt
@@ -4,20 +4,16 @@ import dev.baseio.slackdomain.model.channel.DomainLayerChannels
 import dev.baseio.slackdomain.datasources.local.channels.SKLocalDataSourceCreateChannels
 import dev.baseio.slackdomain.datasources.local.channels.SKLocalDataSourceReadChannels
 import dev.baseio.slackdomain.datasources.remote.channels.SKNetworkDataSourceWriteChannels
-import dev.baseio.slackdomain.usecases.BaseUseCase
 
 class UseCaseCreateChannel(
   private val SKLocalDataSourceCreateChannels: SKLocalDataSourceCreateChannels,
   private val skNetworkDataSourceWriteChannels: SKNetworkDataSourceWriteChannels,
-  private val skLocalDataSourceReadChannels: SKLocalDataSourceReadChannels,
-  private val useCaseInviteUserToChannel: UseCaseInviteUserToChannel
+  private val skLocalDataSourceReadChannels: SKLocalDataSourceReadChannels
 ) {
   suspend operator fun invoke(params: DomainLayerChannels.SKChannel): Result<DomainLayerChannels.SKChannel> {
     return kotlin.runCatching {
       val channel = skNetworkDataSourceWriteChannels.createChannel(params).getOrThrow()
       SKLocalDataSourceCreateChannels.saveChannel(channel)
-      val result = useCaseInviteUserToChannel.addUsersToChannelOnceCreated(channel)
-      result
       skLocalDataSourceReadChannels.getChannelById(channel.workspaceId, channel.channelId)!!
     }
   }
