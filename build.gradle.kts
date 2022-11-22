4038f82bcb13ed8dc42319764365ca03c672b357
anmol verma <anmol.verma4@gmail.com>
2022-11-22 14:11:31 +0530

    setup: native iosApp

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,20 +27,43 @@ kotlin {
         }
     }
 
-    cocoapods {
-        summary = "Some description for the Shared Module"
-        homepage = "Link to the Shared Module homepage"
-        version = "1.0"
-        ios.deploymentTarget = "14.1"
-        podfile = project.file("../iosApp/Podfile")
-        framework {
+    val iosx64 = iosX64()
+    val iosarm64 = iosArm64()
+    val iossimulatorarm64 = iosSimulatorArm64()
+
+    configure(listOf(iosx64, iosarm64,iossimulatorarm64)) {
+        binaries.framework {
             baseName = "slack_domain_layer"
         }
     }
 
-    iosX64()
-    iosArm64()
-    iosSimulatorArm64()
+    // Create a task to build a fat framework.
+    tasks.register<org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask>("debugFatFramework") {
+        // The fat framework must have the same base name as the initial frameworks.
+        baseName = "slack_domain_layer"
+        // The default destination directory is "<build directory>/fat-framework".
+        destinationDir = buildDir.resolve("fat-framework/debug")
+        // Specify the frameworks to be merged.
+        from(
+            iosarm64.binaries.getFramework("DEBUG"),
+            iosx64.binaries.getFramework("DEBUG"),
+            //iossimulatorarm64.binaries.getFramework("DEBUG")
+        )
+    }
+
+    // Create a task to build a fat framework.
+    tasks.register<org.jetbrains.kotlin.gradle.tasks.FatFrameworkTask>("releaseFatFramework") {
+        // The fat framework must have the same base name as the initial frameworks.
+        baseName = "slack_domain_layer"
+        // The default destination directory is "<build directory>/fat-framework".
+        destinationDir = buildDir.resolve("fat-framework/release")
+        // Specify the frameworks to be merged.
+        from(
+            iosarm64.binaries.getFramework("RELEASE"),
+            iosx64.binaries.getFramework("RELEASE"),
+            //iossimulatorarm64.binaries.getFramework("RELEASE")
+        )
+    }
 
     sourceSets {
 
